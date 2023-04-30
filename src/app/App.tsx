b497376d6dc5dd9d9fbb008ba8b89490d0d61ead
dmitrybdrv <incubator-it@protonmail.com>
2023-04-30 18:56:30 +0300

    change BrowserRouter for HashRouter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect } from 'react'
 import { useSelector } from 'react-redux'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom'
 import {
 	AppBar,
 	Button,
@@ -42,7 +42,7 @@ function App() {
 	}
 
 	return (
-		<BrowserRouter>
+		<HashRouter>
 			<div className="App">
 				<ErrorSnackbar/>
 				<AppBar position="static">
@@ -64,7 +64,7 @@ function App() {
 					</Routes>
 				</Container>
 			</div>
-		</BrowserRouter>
+		</HashRouter>
 	)
 }
 
