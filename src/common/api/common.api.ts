65dfca895c04cfff324cfa81df019d174abcf76a
dmitrybdrv <incubator-it@protonmail.com>
2023-04-30 18:33:01 +0300

    add gh-pages

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -4,7 +4,7 @@ export const instance = axios.create({
 	baseURL: 'https://social-network.samuraijs.com/api/1.1/',
 	withCredentials: true,
 	headers: {
-		'API-KEY': '1cdd9f77-c60e-4af5-b194-659e4ebd5d41'
+		'API-KEY': 'a8cce568-8f67-4918-8615-1e9ca1e6617a'
 	}
 })
 
